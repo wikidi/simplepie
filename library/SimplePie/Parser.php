113f8e776779e01e7b8f5c31613fb3938e11e0e2
fanas <fanasew@gmail.com>
2015-10-09 09:58:41 +0200

    Better parser

--- library/SimplePie/Parser.php
+++ library/SimplePie/Parser.php
@@ -48,44 +48,38 @@
  *
  * This class can be overloaded with {@see SimplePie::set_parser_class()}
  *
+ * @author Suchkov Artyom <fanasew@gmail.com>
  * @package SimplePie
  * @subpackage Parsing
  */
 class SimplePie_Parser
 {
+	const PARSE_MEMORY_LIMIT = 128 * 1024 * 1024; //128mb
+
 	var $error_code;
 	var $error_string;
 	var $current_line;
 	var $current_column;
 	var $current_byte;
 	var $separator = ' ';
-	var $namespace = array('');
-	var $element = array('');
-	var $xml_base = array('');
-	var $xml_base_explicit = array(false);
-	var $xml_lang = array('');
-	var $data = array();
-	var $datas = array(array());
+	var $namespace = [''];
+	var $element = [''];
+	var $xml_base = [''];
+	var $xml_base_explicit = [false];
+	var $xml_lang = [''];
+	var $data = [];
+	var $datas = [[]];
 	var $current_xhtml_construct = -1;
 	var $encoding;
 	protected $registry;
 
-	public function set_registry(SimplePie_Registry $registry)
-	{
+	public function set_registry(SimplePie_Registry $registry) {
 		$this->registry = $registry;
 	}
 
-	public function parse(&$data, $encoding)
-	{
+	public function parse(&$data, $encoding) {
 		// Use UTF-8 if we get passed US-ASCII, as every US-ASCII character is a UTF-8 character
-		if (strtoupper($encoding) === 'US-ASCII')
-		{
-			$this->encoding = 'UTF-8';
-		}
-		else
-		{
-			$this->encoding = $encoding;
-		}
+		$this->encoding = strtoupper($encoding) === 'US-ASCII' ? 'UTF-8' : $encoding;
 
 		// Strip BOM:
 		// UTF-32 Big Endian BOM
@@ -129,204 +123,147 @@ class SimplePie_Parser
 			}
 		}
 
-		$return = true;
+		libxml_clear_errors();
+		$xml = new XMLReader();
+		$xml->xml($data);
 
-		static $xml_is_sane = null;
-		if ($xml_is_sane === null)
+		$memoryBefore = memory_get_usage();
+		$counter = 0;
+
+		while (@$xml->read())
 		{
-			$parser_check = xml_parser_create();
-			xml_parse_into_struct($parser_check, '<foo>&amp;</foo>', $values);
-			xml_parser_free($parser_check);
-			$xml_is_sane = isset($values[0]['value']);
-		}
-
-		// Create the parser
-		if ($xml_is_sane)
-		{
-			$xml = xml_parser_create_ns($this->encoding, $this->separator);
-			xml_parser_set_option($xml, XML_OPTION_SKIP_WHITE, 1);
-			xml_parser_set_option($xml, XML_OPTION_CASE_FOLDING, 0);
-			xml_set_object($xml, $this);
-			xml_set_character_data_handler($xml, 'cdata');
-			xml_set_element_handler($xml, 'tag_open', 'tag_close');
-
-			// Parse!
-			if (!xml_parse($xml, $data, true))
-			{
-				$this->error_code = xml_get_error_code($xml);
-				$this->error_string = xml_error_string($this->error_code);
-				$return = false;
-			}
-			$this->current_line = xml_get_current_line_number($xml);
-			$this->current_column = xml_get_current_column_number($xml);
-			$this->current_byte = xml_get_current_byte_index($xml);
-			xml_parser_free($xml);
-			return $return;
-		}
-		else
-		{
-			libxml_clear_errors();
-			$xml = new XMLReader();
-			$xml->xml($data);
-			while (@$xml->read())
-			{
-				switch ($xml->nodeType)
-				{
-
-					case constant('XMLReader::END_ELEMENT'):
-						if ($xml->namespaceURI !== '')
-						{
-							$tagName = $xml->namespaceURI . $this->separator . $xml->localName;
-						}
-						else
-						{
-							$tagName = $xml->localName;
-						}
-						$this->tag_close(null, $tagName);
-						break;
-					case constant('XMLReader::ELEMENT'):
-						$empty = $xml->isEmptyElement;
-						if ($xml->namespaceURI !== '')
-						{
-							$tagName = $xml->namespaceURI . $this->separator . $xml->localName;
-						}
-						else
-						{
-							$tagName = $xml->localName;
-						}
-						$attributes = array();
-						while ($xml->moveToNextAttribute())
-						{
-							if ($xml->namespaceURI !== '')
-							{
-								$attrName = $xml->namespaceURI . $this->separator . $xml->localName;
-							}
-							else
-							{
-								$attrName = $xml->localName;
-							}
-							$attributes[$attrName] = $xml->value;
-						}
-						$this->tag_open(null, $tagName, $attributes);
-						if ($empty)
-						{
-							$this->tag_close(null, $tagName);
-						}
-						break;
-					case constant('XMLReader::TEXT'):
-
-					case constant('XMLReader::CDATA'):
-						$this->cdata(null, $xml->value);
-						break;
-				}
-			}
-			if ($error = libxml_get_last_error())
-			{
-				$this->error_code = $error->code;
-				$this->error_string = $error->message;
-				$this->current_line = $error->line;
-				$this->current_column = $error->column;
+			if ($counter++ % 10 === 0 && memory_get_usage() - $memoryBefore > self::PARSE_MEMORY_LIMIT) {
+				$this->error_string = 'Memory limit exceeded: more than ' . memory_get_usage() . ' bytes used to parse';
 				return false;
 			}
-			else
+
+			switch ($xml->nodeType)
 			{
-				return true;
+				case XMLReader::END_ELEMENT:
+					if ($xml->namespaceURI !== '') {
+						$tagName = $xml->namespaceURI . $this->separator . $xml->localName;
+					} else {
+						$tagName = $xml->localName;
+					}
+					$this->tag_close(null, $tagName);
+					break;
+				case XMLReader::ELEMENT:
+					$empty = $xml->isEmptyElement;
+					if ($xml->namespaceURI !== '') {
+						$tagName = $xml->namespaceURI . $this->separator . $xml->localName;
+					} else {
+						$tagName = $xml->localName;
+					}
+					$attributes = array();
+					while ($xml->moveToNextAttribute()) {
+						if ($xml->namespaceURI !== '') {
+							$attrName = $xml->namespaceURI . $this->separator . $xml->localName;
+						} else {
+							$attrName = $xml->localName;
+						}
+						$attributes[$attrName] = $xml->value;
+					}
+					$this->tag_open(null, $tagName, $attributes);
+					if ($empty) {
+						$this->tag_close(null, $tagName);
+					}
+					break;
+				case XMLReader::TEXT:
+				case XMLReader::CDATA:
+					$this->cdata(null, $xml->value);
+					break;
 			}
 		}
+
+		if ($error = libxml_get_last_error()) {
+			$this->error_code = $error->code;
+			$this->error_string = $error->message;
+			$this->current_line = $error->line;
+			$this->current_column = $error->column;
+			return false;
+		}
+
+		return true;
 	}
 
-	public function get_error_code()
-	{
+	public function get_error_code() {
 		return $this->error_code;
 	}
 
-	public function get_error_string()
-	{
+	public function get_error_string() {
 		return $this->error_string;
 	}
 
-	public function get_current_line()
-	{
+	public function get_current_line() {
 		return $this->current_line;
 	}
 
-	public function get_current_column()
-	{
+	public function get_current_column() {
 		return $this->current_column;
 	}
 
-	public function get_current_byte()
-	{
+	public function get_current_byte() {
 		return $this->current_byte;
 	}
 
-	public function get_data()
-	{
+	public function get_data() {
 		return $this->data;
 	}
 
-	public function tag_open($parser, $tag, $attributes)
-	{
+	public function tag_open($parser, $tag, $attributes) {
 		list($this->namespace[], $this->element[]) = $this->split_ns($tag);
 
-		$attribs = array();
-		foreach ($attributes as $name => $value)
-		{
+		$attribs = [];
+		foreach ($attributes as $name => $value) {
 			list($attrib_namespace, $attribute) = $this->split_ns($name);
 			$attribs[$attrib_namespace][$attribute] = $value;
 		}
 
-		if (isset($attribs[SIMPLEPIE_NAMESPACE_XML]['base']))
-		{
+		if (isset($attribs[SIMPLEPIE_NAMESPACE_XML]['base'])) {
 			$base = $this->registry->call('Misc', 'absolutize_url', array($attribs[SIMPLEPIE_NAMESPACE_XML]['base'], end($this->xml_base)));
 			if ($base !== false)
 			{
 				$this->xml_base[] = $base;
 				$this->xml_base_explicit[] = true;
 			}
-		}
-		else
-		{
+		} else {
 			$this->xml_base[] = end($this->xml_base);
 			$this->xml_base_explicit[] = end($this->xml_base_explicit);
 		}
 
-		if (isset($attribs[SIMPLEPIE_NAMESPACE_XML]['lang']))
-		{
+		if (isset($attribs[SIMPLEPIE_NAMESPACE_XML]['lang'])) {
 			$this->xml_lang[] = $attribs[SIMPLEPIE_NAMESPACE_XML]['lang'];
-		}
-		else
-		{
+		} else {
 			$this->xml_lang[] = end($this->xml_lang);
 		}
 
-		if ($this->current_xhtml_construct >= 0)
-		{
+		if ($this->current_xhtml_construct >= 0) {
 			$this->current_xhtml_construct++;
-			if (end($this->namespace) === SIMPLEPIE_NAMESPACE_XHTML)
-			{
+			if (end($this->namespace) === SIMPLEPIE_NAMESPACE_XHTML) {
 				$this->data['data'] .= '<' . end($this->element);
-				if (isset($attribs['']))
-				{
-					foreach ($attribs[''] as $name => $value)
-					{
+				if (isset($attribs[''])) {
+					foreach ($attribs[''] as $name => $value) {
 						$this->data['data'] .= ' ' . $name . '="' . htmlspecialchars($value, ENT_COMPAT, $this->encoding) . '"';
 					}
 				}
 				$this->data['data'] .= '>';
 			}
-		}
-		else
-		{
+		} else {
 			$this->datas[] =& $this->data;
 			$this->data =& $this->data['child'][end($this->namespace)][end($this->element)][];
-			$this->data = array('data' => '', 'attribs' => $attribs, 'xml_base' => end($this->xml_base), 'xml_base_explicit' => end($this->xml_base_explicit), 'xml_lang' => end($this->xml_lang));
-			if ((end($this->namespace) === SIMPLEPIE_NAMESPACE_ATOM_03 && in_array(end($this->element), array('title', 'tagline', 'copyright', 'info', 'summary', 'content')) && isset($attribs['']['mode']) && $attribs['']['mode'] === 'xml')
-			|| (end($this->namespace) === SIMPLEPIE_NAMESPACE_ATOM_10 && in_array(end($this->element), array('rights', 'subtitle', 'summary', 'info', 'title', 'content')) && isset($attribs['']['type']) && $attribs['']['type'] === 'xhtml')
-			|| (end($this->namespace) === SIMPLEPIE_NAMESPACE_RSS_20 && in_array(end($this->element), array('title')))
-			|| (end($this->namespace) === SIMPLEPIE_NAMESPACE_RSS_090 && in_array(end($this->element), array('title')))
-			|| (end($this->namespace) === SIMPLEPIE_NAMESPACE_RSS_10 && in_array(end($this->element), array('title'))))
-			{
+			$this->data = [
+				'data' => '',
+				'attribs' => $attribs,
+				'xml_base' => end($this->xml_base),
+				'xml_base_explicit' => end($this->xml_base_explicit),
+				'xml_lang' => end($this->xml_lang)
+			];
+			$end = $this->namespace;
+			if (($end === SIMPLEPIE_NAMESPACE_ATOM_03 && in_array($end, ['title', 'tagline', 'copyright', 'info', 'summary', 'content']) && isset($attribs['']['mode']) && $attribs['']['mode'] === 'xml')
+			|| ($end === SIMPLEPIE_NAMESPACE_ATOM_10 && in_array($end, ['rights', 'subtitle', 'summary', 'info', 'title', 'content']) && isset($attribs['']['type']) && $attribs['']['type'] === 'xhtml')
+			|| ($end === SIMPLEPIE_NAMESPACE_RSS_20 && in_array($end, ['title']))
+			|| ($end === SIMPLEPIE_NAMESPACE_RSS_090 && in_array($end, ['title']))
+			|| ($end === SIMPLEPIE_NAMESPACE_RSS_10 && in_array($end, ['title']))) {
 				$this->current_xhtml_construct = 0;
 			}
 		}
@@ -334,23 +271,20 @@ class SimplePie_Parser
 
 	public function cdata($parser, $cdata)
 	{
-		if ($this->current_xhtml_construct >= 0)
-		{
+		if ($this->current_xhtml_construct >= 0) {
 			$this->data['data'] .= htmlspecialchars($cdata, ENT_QUOTES, $this->encoding);
-		}
-		else
-		{
+		} else {
 			$this->data['data'] .= $cdata;
 		}
 	}
 
 	public function tag_close($parser, $tag)
 	{
-		if ($this->current_xhtml_construct >= 0)
-		{
+		if ($this->current_xhtml_construct >= 0) {
 			$this->current_xhtml_construct--;
-			if (end($this->namespace) === SIMPLEPIE_NAMESPACE_XHTML && !in_array(end($this->element), array('area', 'base', 'basefont', 'br', 'col', 'frame', 'hr', 'img', 'input', 'isindex', 'link', 'meta', 'param')))
-			{
+			if (end($this->namespace) === SIMPLEPIE_NAMESPACE_XHTML && !in_array(end($this->element), [
+						'area', 'base', 'basefont', 'br', 'col', 'frame', 'hr', 'img', 'input', 'isindex', 'link', 'meta', 'param'
+					])) {
 				$this->data['data'] .= '</' . end($this->element) . '>';
 			}
 		}
